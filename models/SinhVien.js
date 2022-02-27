31c3671075ba51b126c8786d9cc32805004414d9
unknown <lemauduyphuong008@gmail.com>
2022-02-27 09:59:50 +0700

    update_hienthithongtin

--- models/SinhVien.js
+++ models/SinhVien.js
@@ -1,18 +1,20 @@
 //Tạo ra kiểu dữ liệu (prototype | class ) để lưu trữ thông tin từ giao diện
-function SinhVien () {
-    this.maSinhVien = '';
-    this.tenSinhVien = '';
-    this.loaiSinhVien = '';
-    this.diemToan = 0;
-    this.diemLy = 0;
-    this.diemHoa = 0;
-    this.diemRenLuyen =0;
-    this.email = '';
-    this.soDienThoai ='';
-    this.tinhDiemTrungBinh = function () {
-        var dtb = (Number(this.diemHoa) + Number(this.diemLy) + Number(this.diemToan))
-        return dtb;
-    }
-
+function SinhVien() {
+  this.maSinhVien = "";
+  this.tenSinhVien = "";
+  this.loaiSinhVien = "";
+  this.diemToan = 0;
+  this.diemLy = 0;
+  this.diemHoa = 0;
+  this.diemRenLuyen = 0;
+  this.email = "";
+  this.soDienThoai = "";
+  this.tinhDiemTrungBinh = function () {
+    var dtb =
+      Number(this.diemHoa) + Number(this.diemLy) + Number(this.diemToan);
+    return dtb;
+  };
+  this.hienThiThongTin = function () {
+    console.log("hien thi thong tin");
+  };
 }
-
